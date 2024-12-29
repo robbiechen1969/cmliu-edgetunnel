c5d6b539276f9bd838b4aa1444e99148eb481a84
i520e3e4 <46157991+i520e3e4@users.noreply.github.com>
2024-12-29 18:23:14 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '9debcb29-c317-449d-b225-388cb4db8db6';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
