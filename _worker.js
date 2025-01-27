dd9c6da4b94e000a77cd9721c11d962e07f6353e
PERSIA-MISI <124380548+PERSIA-MISI@users.noreply.github.com>
2025-01-27 07:16:52 +0330

    Update _worker.js
    77

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = '';
+let proxyIP = '77.232.142.61';
 //let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
